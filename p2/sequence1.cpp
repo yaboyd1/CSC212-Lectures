b5408a2a2f77f7549e829578d87eb5ffbb82cfd0
Dewan <dtahmid000@citymail.cuny.edu>
2020-07-10 22:01:00 -0400

    Finished insert, attach and remove functions

--- p2/sequence1.cpp
+++ p2/sequence1.cpp
@@ -2,36 +2,55 @@
 #include "sequence1.h"
 
 namespace main_savitch_3 {
-	typedef double value_type;
-	typedef std::size_t size_type;
-	static const size_type CAPACITY = 30;
 	// CONSTRUCTOR
 	sequence() {
 		used = current_index = 0;
 	}
+
 	// MODIFICATION MEMBER FUNCTIONS
 	void start() {
 		if (used != 0) current_index = 0;
 	}
+
 	void advance() {
 		assert(is_item());
 		++current_index;
 	}
+
 	void insert(const value_type& entry) {
 		assert(size() < CAPACITY);
+		if (!is_item()) current_index = 0; /* Insert at the start of the sequence at default */
+		for (sequence1::size_type i = used; i > current_index; --i) 
+			data[i] = data[i - 1]; /* Shift elements forward to make room */
+		data[current_index] = entry;
+		++used;
 	}
+
 	void attach(const value_type& entry) {
 		assert(size() < CAPACITY);
+		if (!is_item()) current_index = used - 1; /* Insert at the end of the sequence at default */
+		for (sequence1::size_type i = used; i > current_index + 1; --i) 
+			data[i] = data[i - 1]; /* Shift elements forward to make room */
+		data[++current_index] = entry;
+		++used;
 	}
-	void remove_current();
+
+	void remove_current() {
+		assert(is_item());
+		for (sequence1::size_type i = current_index + 1; i < used; ++i) 
+			data[i - 1] = data[i]; /* Overwrite the data */
+		--used;
+	}
+
 	// CONSTANT MEMBER FUNCTIONS
 	size_type size() const {
-		assert(is_item());
 		return used;
 	}
+
 	bool is_item() const {
 		return used != 0;
 	}
+
 	value_type current() const {
 		assert(is_item());
 		return data[current_index];
