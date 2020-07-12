828ae40de22888d2aef0aa4f1d78a59e6d607712
Dewan <dtahmid000@citymail.cuny.edu>
2020-07-12 16:22:00 -0400

    Modified skeleton to include big three

--- p3/sequence2.cpp
+++ p3/sequence2.cpp
@@ -1,23 +1,33 @@
 #include <cassert> // Provides assert
-#include "sequence1.h"
+#include "sequence2.h"
 
-namespace main_savitch_3 {
+namespace main_savitch_4 {
 	// CONSTRUCTOR
-	sequence() {
+	sequence::sequence(size_type initial_capacity = DEFAULT_CAPACITY) {
 		used = current_index = 0;
+		capacity = initial_capacity;
+		data = new sequence::value_type[capacity];
+	}
+
+	sequence::sequence(const sequence& source) {
+
+	}
+
+	sequence::~sequence() {
+
 	}
 
 	// MODIFICATION MEMBER FUNCTIONS
-	void start() {
+	void sequence::start() {
 		if (used != 0) current_index = 0;
 	}
 
-	void advance() {
+	void sequence::advance() {
 		assert(is_item());
 		++current_index;
 	}
 
-	void insert(const value_type& entry) {
+	void sequence::insert(const value_type& entry) {
 		assert(size() < CAPACITY);
 		if (!is_item()) current_index = 0; /* Insert at the start of the sequence at default */
 		for (sequence1::size_type i = used; i > current_index; --i)
@@ -26,7 +36,7 @@ namespace main_savitch_3 {
 		++used;
 	}
 
-	void attach(const value_type& entry) {
+	void sequence::attach(const value_type& entry) {
 		assert(size() < CAPACITY);
 		if (!is_item()) current_index = used - 1; /* Insert at the end of the sequence at default */
 		for (sequence1::size_type i = used; i > current_index + 1; --i)
@@ -35,23 +45,27 @@ namespace main_savitch_3 {
 		++used;
 	}
 
-	void remove_current() {
+	void sequence::remove_current() {
 		assert(is_item());
 		for (sequence1::size_type i = current_index + 1; i < used; ++i)
 			data[i - 1] = data[i]; /* Overwrite the data */
 		--used;
 	}
 
+	void operator =(const sequence& source) {
+
+	}
+
 	// CONSTANT MEMBER FUNCTIONS
-	size_type size() const {
+	sequence::size_type sequence::size() const {
 		return used;
 	}
 
-	bool is_item() const {
+	bool sequence::is_item() const {
 		return used != 0;
 	}
 
-	value_type current() const {
+	sequence:value_type sequence::current() const {
 		assert(is_item());
 		return data[current_index];
 	}
