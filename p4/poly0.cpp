c45a3609ee2e444fbc7ebad8b396daa1e9098e51
Dewan <dtahmid000@citymail.cuny.edu>
2020-07-14 15:28:43 -0400

    Implemented constructor and modification member functions

--- p4/poly0.cpp
+++ p4/poly0.cpp
@@ -1,14 +1,35 @@
+#include <cassert>   // Provides assert()
+#include <algorithm> // Provides fill_n()
+using namespace std;
 #include "poly0.h"
 
 namespace main_savitch_3
 {
+	// CONSTANTS
+	static const unsigned int polynomial::CAPACITY;
+	static const unsigned int polynomial::MAX_EX = CAPACITY;
+
 	// CONSTRUCTOR
-	polynomial::polynomial(double c, unsigned int exponent);
+	polynomial::polynomial(double c, unsigned int exponent) {
+		assert(exponent <= MAX_EX);
+		clear();
+		current_degree = 0;
+		assign_coef(c, exponent);
+	}
 
 	// MODIFICATION MEMBER FUNCTIONS
-	void polynomial::add_to_coef(double amount, unsigned int exponent);
-	void polynomial::assign_coef(double coefficient, unsigned int exponent);
-	void polynomial::clear();
+	void polynomial::add_to_coef(double amount, unsigned int exponent) {
+		assert(exponent <= MAX_EX);
+		assign_coef(coef[exponent] + amount, exponent);
+	}
+	void polynomial::assign_coef(double coefficient, unsigned int exponent) {
+		assert(exponent <= MAX_EX);
+		coef[exponent] = coefficient;
+		/* Current Degree will be done later */
+	}
+	void polynomial::clear() {
+		fill_n(coef, CAPACITY, 0.0);
+	}
 
 	// CONSTANT MEMBER FUNCTIONS
 	double polynomial::coefficient(unsigned int exponent) const;
