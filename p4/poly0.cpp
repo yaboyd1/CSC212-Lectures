b0f6d57eb4ed176d81077920274cc3186c3bd262
Dewan <dtahmid000@citymail.cuny.edu>
2020-07-14 16:21:56 -0400

    Implemented most functions

--- p4/poly0.cpp
+++ p4/poly0.cpp
@@ -1,13 +1,14 @@
 #include <cassert>   // Provides assert()
 #include <algorithm> // Provides fill_n()
+#include <climits>   // Provides UINT_MAX
 using namespace std;
 #include "poly0.h"
 
 namespace main_savitch_3
 {
 	// CONSTANTS
-	static const unsigned int polynomial::CAPACITY;
-	static const unsigned int polynomial::MAX_EX = CAPACITY;
+	const unsigned int polynomial::CAPACITY;
+	const unsigned int polynomial::MAX_EX;
 
 	// CONSTRUCTOR
 	polynomial::polynomial(double c, unsigned int exponent) {
@@ -22,29 +23,93 @@ namespace main_savitch_3
 		assert(exponent <= MAX_EX);
 		assign_coef(coef[exponent] + amount, exponent);
 	}
+
 	void polynomial::assign_coef(double coefficient, unsigned int exponent) {
 		assert(exponent <= MAX_EX);
 		coef[exponent] = coefficient;
-		/* Current Degree will be done later */
+		/* Set new Current Degree */
+		if (exponent > current_degree) current_degree = exponent;
+		if (coef[current_degree] == 0) {
+			for (unsigned int i = current_degree; i > 0 && coef[i] != 0; --i) {
+				--current_degree;
+			}
+		}
 	}
+
 	void polynomial::clear() {
 		fill_n(coef, CAPACITY, 0.0);
+		current_degree = 0;
 	}
 
 	// CONSTANT MEMBER FUNCTIONS
-	double polynomial::coefficient(unsigned int exponent) const;
-	polynomial polynomial::derivative() const;
-	double polynomial::eval(double x) const;
-	unsigned int polynomial::next_term(unsigned int e) const;
-	unsigned int polynomial::previous_term(unsigned int e) const;
+	double polynomial::coefficient(unsigned int exponent) const {
+		if (exponent > MAX_EX) return 0;
+		return coef[exponent];
+	}
+
+	/* Just for now until I figure this out */
+	polynomial polynomial::derivative() const {
+		return *this;
+	}
+
+	double polynomial::eval(double x) const{
+		return 0;
+	}
+
+	unsigned int polynomial::next_term(unsigned int e) const {
+		for (unsigned int i = e + 1; i != MAX_EX; ++i) {
+			if (coefficient(i) != 0) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	unsigned int polynomial::previous_term(unsigned int e) const {
+		for (unsigned int i = e - 1; i > 0; --i) {
+			if (coefficient(i) != 0) {
+				return i;
+			}
+		}
+		return UINT_MAX;
+	}
 
 	// NON-MEMBER BINARY OPERATORS
-	polynomial operator +(const polynomial& p1, const polynomial& p2);
-	polynomial operator -(const polynomial& p1, const polynomial& p2);
-	polynomial operator *(const polynomial& p1, const polynomial& p2);
+	polynomial operator +(const polynomial& p1, const polynomial& p2) {
+		if (p1.degree() == 0) return p2;
+		if (p2.degree() == 0) return p1;
+		polynomial max, min;
+		p1.degree() > p2.degree() ? max = p1, min = p2 : max = p2, min = p1;
+		for (unsigned int i = 0; i != min.degree(); ++i) {
+			max.add_to_coef(min.coefficient(i), i);
+		}
+		return max;
+	}
+
+	polynomial operator -(const polynomial& p1, const polynomial& p2) {
+		if (p1.degree() == 0) return p2;
+		if (p2.degree() == 0) return p1;
+		polynomial max, min;
+		p1.degree() > p2.degree() ? max = p1, min = p2 : max = p2, min = p1;
+		for (unsigned int i = 0; i != min.degree(); ++i) {
+			max.add_to_coef(-min.coefficient(i), i);
+		}
+		return max;
+	}
+
+	/* Just for now until I figure this out */
+	polynomial operator *(const polynomial& p1, const polynomial& p2) {
+		assert(p1.degree() + p2.degree() <= polynomial::MAX_EX);
+		return polynomial(p1);
+	}
 
 	// NON-MEMBER OUTPUT FUNCTIONS
-	std::ostream& operator << (std::ostream& out, const polynomial& p);
+
+	/* Just for now until I figure this out */
+	std::ostream& operator << (std::ostream& out, const polynomial& p) {
+		return out << "Hi";
+	}
+
 	void make_gif(
 		const polynomial& p,
 		const char filename[ ],
