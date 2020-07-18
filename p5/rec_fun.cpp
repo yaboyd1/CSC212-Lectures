7962126b7cd4d80c6754119d5e2ec28ad5b91f02
Dewan <dtahmid000@citymail.cuny.edu>
2020-07-17 20:05:12 -0400

    First recursive problem added

---
+++ p5/rec_fun.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void triangle(ostream& outs, unsigned int m, unsigned int n) {
+// Precondition: m <= n
+// Postcondition: The function has printed a pattern of 2*(n-m+1) lines
+// to the output stream outs. The first line contains m asterisks, the next 
+// line contains m+1 asterisks, and so on up to a line with n asterisks.
+// Then the pattern is repeated backwards, going n back down to m.
+/* Example output:
+ triangle(cout, 3, 5) will print this to cout:
+ ***
+ ****
+ *****
+ *****
+ ****
+ ***
+*/
+}
+
+int main() {
+	return 0;
+}
