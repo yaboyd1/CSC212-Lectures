414b2977748477de70df228d31dff7628d01a43f
Dewan <dtahmid000@citymail.cuny.edu>
2020-07-20 21:24:11 -0400

    Added most constructors and simple mod and const function

--- p6/poly1.cpp
+++ p6/poly1.cpp
@@ -1,19 +1,60 @@
-#include <cstdlib>  // Provides size_t 
-#include <iostream> // Provides cout
+#include <cstdlib>   // Provides size_t 
+#include <iostream>  // Provides cout
+#include <algorithm> // Provides fill_n() and copy()
 #include "poly1.h"
 
 namespace main_savitch_4 {
+	// CONSTANTS	
+	const unsigned int polynomial::DEFAULT_CAPACITY;
+
 	// CONSTRUCTORS and DESTRUCTOR
-	polynomial::polynomial();
-	polynomial::polynomial(const polynomial& source);
-	polynomial::polynomial(double c, unsigned int exponent);
-	polynomial::~polynomial();
+	polynomial::polynomial() {
+		current_array_size = DEFAULT_CAPACITY;
+		clear();
+	}
+
+	polynomial::polynomial(const polynomial& source) {
+		current_array_size = source.current_array_size;
+		current_degree = source.current_degree;
+		coef = new double[current_array_size];
+		copy(source.coef, source.coef + current_degree + 1, coef);
+	}
+
+	polynomial::polynomial(double c, unsigned int exponent) {
+		current_array_size = DEFAULT_CAPACITY;
+		clear();
+		assign_coef(c, exponent);
+	}
+
+	polynomial::~polynomial() {
+		delete [] coef;
+	}
 
 	// MODIFICATION MEMBER FUNCTIONS
-	void polynomial::add_to_coef(double amount, unsigned int exponent);
-	void polynomial::assign_coef(double coefficient, unsigned int exponent);
-	void polynomial::clear();
-	void polynomial::reserve(size_t number);
+	void polynomial::add_to_coef(double amount, unsigned int exponent) {
+		if (current_degree + 1 == current_array_size) reserve(current_array_size + 1);
+		coef[exponent] += amount;
+	}
+	void polynomial::assign_coef(double coefficient, unsigned int exponent) {
+		if (current_degree + 1 == current_array_size) reserve(current_array_size + 1);
+		coef[exponent] = coefficient;
+	}
+
+	void polynomial::clear() {
+		fill_n(coef, current_array_size, 0.0);
+		current_degree = 0;
+	}
+
+	void polynomial::reserve(size_t new_size) {
+		if (new_size == current_array_size) return;
+		if (new_size < current_degree + 1) new_size = current_degree;
+		double *new_coef = new double[new_size];
+		copy(coef, coef + current_degree + 1, new_coef);
+		delete [] coef;
+		coef = new_coef;
+		current_array_size = new_size;
+	}
+
 	void polynomial::trim();
 
 	// MODIFICATION OPERATORS
@@ -24,10 +65,18 @@ namespace main_savitch_4 {
 	polynomial& polynomial::operator *=(double c);
 
 	// CONSTANT MEMBER FUNCTIONS
-	double polynomial::coefficient(unsigned int exponent) const;
+	double polynomial::coefficient(unsigned int exponent) const {
+		return coef[exponent];
+	}
+
 	double polynomial::definite_integral(double low_bound, double high_bound) const;
-	unsigned int polynomial::degree() const;
+
+	unsigned int polynomial::degree() const {
+		return current_degree;
+	}
+
 	polynomial polynomial::derivative(unsigned int n) const;
+
 	double polynomial::eval(double x) const;
 
 	void polynomial::find_root(
